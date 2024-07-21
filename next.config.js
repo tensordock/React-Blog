41326c3d8dbd8760e77be3c70c1aa847e6148562
ntu2323 <64795953+ntu2323@users.noreply.github.com>
2024-07-20 17:15:45 -0700

    bug fixes

--- next.config.js
+++ next.config.js
@@ -60,7 +60,6 @@ const securityHeaders = [
 module.exports = () => {
   const plugins = [withContentlayer, withBundleAnalyzer]
   return plugins.reduce((acc, next) => next(acc), {
-    basePath: "tensordock/React-Blog",
     output: 'export',
     reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
